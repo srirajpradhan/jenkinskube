51cdc57f2071216161ea8c24d026ba58a80ea3fb
root <root@ip-172-31-30-1.us-east-2.compute.internal>
2018-10-24 10:57:42 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM maven:3.5-jdk-8-alpine
+
+ENV HOME /Javaapp
+EXPOSE 9669
+
+WORKDIR $HOME
+
+COPY . $HOME/
+
+RUN mvn clean install \
+    && apk add --update bash \
+    && rm -rf /var/cache/apk/*
+
+CMD ["java","-jar","/Javaapp/target/spring-boot-web-0.0.2-SNAPSHOT.jar"]
---
+++ src/main/java/com/ensat/entities/Product.java
@@ -0,0 +1,65 @@
+package com.ensat.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    private String productId;
+    private String name;
+    
+   
+    private Integer price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+}
